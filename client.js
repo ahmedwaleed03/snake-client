2a85a951a0ac7f10158fe770349f512b6b74f7cb
Waleed Ahmed <baloch.waleedahmed@gmail.com>
2023-08-30 14:31:19 -0400

    added movement

--- client.js
+++ client.js
@@ -12,11 +12,6 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: WAL");
-    // conn.write("Move: up");
-    // conn.write("Move: up");
-    // setTimeout(() => {
-    //   conn.write("Move: up")
-    // }, 50);
   });
   conn.on("data", (data) => {
     console.log(data);
--- input.js
+++ input.js
@@ -1,11 +1,24 @@
+// Stores the active TCP connection object.
+let connection;
+
 const handleUserInput = function (data) {
   // your code here
   if (data === '\u0003') {
     process.exit();
   }
+  if (data === 'w') {
+    connection.write("Move: up");
+  } else if (data === 'a') {
+    connection.write("Move: left");
+  } else if (data === 's') {
+    connection.write("Move: down");
+  } else if (data === 'd') {
+    connection.write("Move: right");
+  }
 };
 
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
