e9d841141089c7ee3ee308fdd42a170cb8c48c96
Waleed Ahmed <baloch.waleedahmed@gmail.com>
2023-08-30 13:45:57 -0400

    added name to the snek

--- client.js
+++ client.js
@@ -9,8 +9,11 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
-  conn.on('data', (data) => {
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: WAL");
+  });
+  conn.on("data", (data) => {
     console.log(data);
   });
 
