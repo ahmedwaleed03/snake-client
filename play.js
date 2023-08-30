92e512879d4d145053a1904c34b65104ecb1e51f
Waleed Ahmed <baloch.waleedahmed@gmail.com>
2023-08-30 14:09:57 -0400

    moved input functionality to input.js

--- play.js
+++ play.js
@@ -1,22 +1,7 @@
-const client = require("./client");
-
-const handleUserInput = function (data) {
-  // your code here
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+const {connect} = require("./client");
+const {setupInput} = require("./input");
 
 console.log("Connecting ...");
-client.connect();
+connect();
 
 setupInput();
